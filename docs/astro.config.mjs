ab506844e5b573f957dfab542b798593b871c768
Serenacula <26283806+Serenacula@users.noreply.github.com>
2026-01-24 20:52:32 +0000

    adjusting sidebar structure

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -7,7 +7,7 @@ import starlightAutoSidebar from "starlight-auto-sidebar"
 export default defineConfig({
     integrations: [
         starlight({
-            plugins: [starlightAutoSidebar()],
+            // plugins: [starlightAutoSidebar()],
             title: "splitby",
             social: [
                 {
@@ -28,92 +28,124 @@ export default defineConfig({
                 },
                 {
                     label: "Flags",
-                    autogenerate: {
-                        directory: "flags",
-                    },
+                    items: [
+                        { label: "Overview", link: "/flags/" },
+                        {
+                            label: "File Flags",
+                            items: [
+                                {
+                                    label: "Input",
+                                    link: "/flags/file_flags/input",
+                                },
+                                {
+                                    label: "Output",
+                                    link: "/flags/file_flags/output",
+                                },
+                            ],
+                        },
+                        {
+                            label: "Input Modes",
+                            items: [
+                                {
+                                    label: "Per line",
+                                    link: "/flags/input_modes/per-line",
+                                },
+                                {
+                                    label: "Whole string",
+                                    link: "/flags/input_modes/whole-string",
+                                },
+                                {
+                                    label: "Zero terminated",
+                                    link: "/flags/input_modes/zero-terminated",
+                                },
+                            ],
+                        },
+                        {
+                            label: "Selection Modes",
+                            items: [
+                                {
+                                    label: "Fields",
+                                    link: "/flags/selection_modes/fields",
+                                },
+                                {
+                                    label: "Bytes",
+                                    link: "/flags/selection_modes/bytes",
+                                },
+                                {
+                                    label: "Characters",
+                                    link: "/flags/selection_modes/characters",
+                                },
+                            ],
+                        },
+                        {
+                            label: "Selection Flags",
+                            items: [
+                                {
+                                    label: "Delimiter",
+                                    link: "/flags/selection_flags/delimiter",
+                                },
+                                {
+                                    label: "Invert",
+                                    link: "/flags/selection_flags/invert",
+                                },
+                                {
+                                    label: "Skip empty",
+                                    link: "/flags/selection_flags/skip-empty",
+                                },
+                            ],
+                        },
+                        {
+                            label: "Transform Flags",
+                            items: [
+                                {
+                                    label: "Align",
+                                    link: "/flags/transform_flags/align",
+                                },
+                                {
+                                    label: "Count",
+                                    link: "/flags/transform_flags/count",
+                                },
+                                {
+                                    label: "Join",
+                                    link: "/flags/transform_flags/join",
+                                },
+                                {
+                                    label: "Placeholder",
+                                    link: "/flags/transform_flags/placeholder",
+                                },
+                            ],
+                        },
+                        {
+                            label: "Strict Flags",
+                            items: [
+                                {
+                                    label: "Strict",
+                                    link: "/flags/strict_flags/strict",
+                                },
+                                {
+                                    label: "Strict bounds",
+                                    link: "/flags/strict_flags/strict-bounds",
+                                },
+                                {
+                                    label: "Strict return",
+                                    link: "/flags/strict_flags/strict-return",
+                                },
+                                {
+                                    label: "Strict range order",
+                                    link: "/flags/strict_flags/strict-range-order",
+                                },
+                                {
+                                    label: "Strict UTF-8",
+                                    link: "/flags/strict_flags/strict-utf8",
+                                },
+                            ],
+                        },
+                    ],
                 },
-                // {
-                //     label: "Modes",
-                //     items: [{ label: "Modes", link: "/modes/" }],
-                // },
-                // {
-                //     label: "Flags",
-                //     items: [
-                //         { label: "Overview", link: "/flags/" },
-                //         {
-                //             label: "Delimiter (-d, --delimiter)",
-                //             link: "/flags/delimiter/",
-                //         },
-                //         { label: "Join (-j, --join)", link: "/flags/join/" },
-                //         {
-                //             label: "Skip empty (-e, -E)",
-                //             link: "/flags/skip-empty/",
-                //         },
-                //         { label: "Invert (--invert)", link: "/flags/invert/" },
-                //         { label: "Count (--count)", link: "/flags/count/" },
-                //         {
-                //             label: "Placeholder (--placeholder)",
-                //             link: "/flags/placeholder/",
-                //         },
-                //         {
-                //             label: "Trim newline (--trim-newline)",
-                //             link: "/flags/trim-newline/",
-                //         },
-                //         {
-                //             label: "Strict (--strict, --no-strict)",
-                //             link: "/flags/strict/",
-                //         },
-                //         {
-                //             label: "Strict bounds (--strict-bounds)",
-                //             link: "/flags/strict-bounds/",
-                //         },
-                //         {
-                //             label: "Strict return (--strict-return)",
-                //             link: "/flags/strict-return/",
-                //         },
-                //         {
-                //             label: "Strict range order (--strict-range-order)",
-                //             link: "/flags/strict-range-order/",
-                //         },
-                //         {
-                //             label: "Strict UTF-8 (--strict-utf8)",
-                //             link: "/flags/strict-utf8/",
-                //         },
-                //         {
-                //             label: "Per-line (--per-line)",
-                //             link: "/flags/per-line/",
-                //         },
-                //         {
-                //             label: "Whole string (-w, --whole-string)",
-                //             link: "/flags/whole-string/",
-                //         },
-                //         {
-                //             label: "Zero terminated (-z, --zero-terminated)",
-                //             link: "/flags/zero-terminated/",
-                //         },
-                //         {
-                //             label: "Input file (-i, --input)",
-                //             link: "/flags/input/",
-                //         },
-                //         {
-                //             label: "Output file (-o, --output)",
-                //             link: "/flags/output/",
-                //         },
-                //         {
-                //             label: "Fields (-f, --fields)",
-                //             link: "/flags/fields/",
-                //         },
-                //         { label: "Bytes (-b, --bytes)", link: "/flags/bytes/" },
-                //         {
-                //             label: "Characters (-c, --characters)",
-                //             link: "/flags/characters/",
-                //         },
-                //     ],
-                // },
-                // {
-                //     label: "FAQ",
-                //     items: [{ label: "FAQ", link: "/faq/" }],
-                // },
+                {
+                    label: "FAQ",
+                    items: [{ label: "FAQ", link: "/faq/" }],
+                },
             ],
         }),
     ],
